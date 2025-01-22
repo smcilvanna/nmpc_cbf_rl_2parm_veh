ef576a7d2c43b62e258c2094ab8d7ff373009cf3
Stephen McIlvanna <smcilvanna01@qub.ac.uk>
2025-01-22 13:33:45 +0000

    inwork update

--- batch_process.m
+++ batch_process.m
@@ -1,4 +1,8 @@
+cd("C:\Users\14244039\OneDrive - Queen's University Belfast\Documents\MATLAB\cbfrl\cbfrl_2param\cbf_2parm_veh")
+return
 
+%%
+load("250121_sweep6.mat")
 
 %% Read in all simulated data and get reward from each run
 
@@ -8,8 +12,15 @@ for i = 1:size(alldata,1)
     
     cbf = alldata(i).cbf;
     obs = alldata(i).obstacle(3);
-    reward = getReward(alldata(i));
-    results = [results ; array2table([cbf',obs,reward],"VariableNames",["k1","k2","rcbf","obs_rad","reward"])];
+    rewardout = getReward(alldata(i));
+    reward = rewardout.reward;
+    dist = rewardout.pathDist;
+    opdst = rewardout.optDist;
+    fsep = rewardout.finishSep;
+    msep = rewardout.min_sep;
+
+    results = [results ; array2table(   [cbf',            obs,      reward,  dist,      opdst,        fsep,       msep], ...
+                        "VariableNames",["k1","k2","rcbf","obs_rad","reward","pathDist","optimalDist","finishSep","minSep"])];
     
     if mod(i,1000)==0
         disp(i)
@@ -33,10 +44,8 @@ for i = 1:numel(orads)
     plotResults(ftable)
     subtitle(sprintf("Obstacle Radius %.03f m",obs))
     % input("ENTER for next figure")
-    input();
+    pause(1);
     clf(fig);
-    
-
 end
 
 
@@ -46,7 +55,140 @@ close all
 ftable = results(ismembertol(results.obs_rad, 2.0, 1e-5),:);
 plotResults(ftable)
 
+
+
+%% Plot valid path distances
+close all;
+orads = unique(results.obs_rad);
+cbfrange = [min(results.k1) max(results.k1)];
+
+fig = figure();
+for i = 1:numel(orads)
+    obs = orads(i);                                                     % new obstacle radius each loop
+    ftable = results(ismembertol(results.obs_rad, obs, 1e-5),:);        % filter table for obstacle
+    avoid_ftable = ftable(ftable.minSep > 0 ,:);                        % filter obstacle table for non collisions
+    %avoid_ftable = sortrows(avoid_ftable,"pathDist");                   
+
+    x = avoid_ftable.k1;
+    y = avoid_ftable.k2;
+    z1 = 1 - normalize(avoid_ftable.pathDist, 'range');
+    z2 = 1 - normalize(avoid_ftable.finishSep, "range");
+    z3 = 1 - normalize(avoid_ftable.minSep,"range")'
+
+    % Create tiled layout
+    t = tiledlayout(2, 2);
+    
+    % Plot 1
+    nexttile;
+    scatter3(x, y, z1, 5, "blue", "filled");
+    title('Path Distance');
+    xlim(cbfrange);
+    ylim(cbfrange);
+    zlim([0 1]);
+    
+    % Plot 2
+    nexttile;
+    scatter3(x, y, z2, 5, "green", "filled");
+    title('Target Seperation');
+    xlim(cbfrange);
+    ylim(cbfrange);
+    zlim([0 1]);
+
+    % Plot 3
+    nexttile;
+    scatter3(x, y, z3, 5, "red", "filled");
+    title('Obstacle Clearance');
+    xlim(cbfrange);
+    ylim(cbfrange);
+    zlim([0 1]);
+
+
+    title(t, "Reward Function Components")
+    subtitle(t, sprintf("Obstacle Radius %.03f m \n1 is better",obs))
+    % input("ENTER for next figure")
+    pause(3);
+    clf(fig);
+end
+
+
+
+
+
+
+
+
+
+
+
+%% Plot Paths
+close all;
+fig = figure(Visible="off");
+
+for i = 1:size(alldata,1)
+    plotPath(fig,alldata(i),0.5);
+    pause(0.1);
+end
+
+
+
+
+
+
 %% LOCAL FUNCTIONS
+
+function plotPath(fig, simdata, orad)
+% PLOTPATH Plot the path of a simulated run
+%   result = plot on fig object of path from simdata
+%   inputs:
+%       fig - input figure object to draw the plot on
+%       simdata - input simulation data 
+%       orad - (Optional) Filter by obstacle radius
+%   Example:
+%       plotPath(fig,simdata,0.5)
+
+    or = simdata.obstacle(3);
+    if or < 0.01
+        return
+    end
+
+    if nargin > 2
+        if abs(or - orad) > 1e-3    % if our set radius matches sim radius
+            return
+        end
+
+    end
+
+    figure(fig);
+    clf(fig)
+    x = simdata.states(1,:);
+    y = simdata.states(2,:);
+
+    xt = simdata.target(1);
+    yt = simdata.target(2);
+
+    ox = simdata.obstacle(1);
+    oy = simdata.obstacle(2);
+    oc = [ox,oy];
+   
+
+    plot(x, y);
+    hold on;
+    scatter(xt, yt, 'MarkerEdgeColor', 'green', 'MarkerFaceColor', 'green', 'Marker', 'x');
+    plotCircle(oc,or,'-','r');
+
+    xlim([-5 , 25]);
+    ylim([-5 , 25]);
+    axis square;
+
+    ttxt = sprintf('k1 = %.3f | k2 = %.3f',simdata.cbf(1),simdata.cbf(2));
+    title(ttxt);
+
+
+    hold off
+    exportgraphics(gcf, './out.gif', 'ContentType', 'image', 'Append', true);
+
+end
+
 
 function plotResults(ftable)
 
@@ -88,3 +230,4 @@ function plotResults(ftable)
     title('Reward Heatmap')
 
 end
+
--- getReward.m
+++ getReward.m
@@ -1,17 +1,19 @@
-function reward = getReward(simdata)
+function rewardout = getReward(simdata)
 % From the simdata, calculate the reward value passed to RL
     min_sep = min(simdata.sep);
-
     if min_sep < 0
         reward = -1;
+        optDist = -1;
+        pathDist = -1;
+        finishSep = -1;
     else
-
         tx = simdata.target(1);
         ty = simdata.target(2);
         ox = simdata.obstacle(1);
         oy = simdata.obstacle(2);
         orad = simdata.obstacle(3);
         vrad = simdata.vrad;
+        
         % Calculate absolute minimum path to clear
         optrad = orad+vrad;
         opx = ox + optrad*cos(deg2rad(135));
@@ -19,6 +21,7 @@ function reward = getReward(simdata)
         l1 = sqrt(opx^2 + opy^2);
         l2 = sqrt( (tx-opx)^2 + (ty-opy)^2 );
         optDist = l1 + l2;
+        
         % Calculate path travelled
         diffs = diff(simdata.states(1:2,:),1,2);
         distances = sqrt(sum(diffs.^2,1));
@@ -32,12 +35,12 @@ function reward = getReward(simdata)
         finishSep = norm([tx ty] - simdata.states(1:2,end));
 
         if reward > 0 && finishSep > 0.5
-            
             reward = reward * 0.5/finishSep;
         end
-
-
     end
-
-
+    rewardout.reward = reward;
+    rewardout.min_sep = min_sep;
+    rewardout.optDist = optDist;
+    rewardout.pathDist = pathDist;
+    rewardout.finishSep = finishSep;
 end
