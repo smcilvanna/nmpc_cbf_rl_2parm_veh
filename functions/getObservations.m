b09000f0d007f3187c4a007e4d3b73c1dcaca976
Stephen McIlvanna <smcilvanna01@qub.ac.uk>
2025-04-16 09:22:54 +0100

    new function to return observations from simdata for training

---
+++ functions/getObservations.m
@@ -0,0 +1,8 @@
+function out = getObservations(simdata)
+%GETNORMALISEDOBSERVATIONS Summary of this function goes here
+%   Detailed explanation goes here
+    
+    rObservations = struct; % struct for the real observations
+    nObservations = struct; % struct for the normalised observations
+
+end
